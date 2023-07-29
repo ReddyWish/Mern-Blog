2b85932f75df09eb064a50be2a3b2a03de9c7f8c
Ilya <ilya.shuvatov1@gmail.com>
2023-07-29 10:18:44 +0200

    finished initial part of frontend and backend

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const allowedOrigins = ["https://somesite.com", "https://zenquotes.io/api/random", "http://localhost:3600", "http://localhost:5174", "http://localhost:5173"];
+
+module.exports = allowedOrigins;
